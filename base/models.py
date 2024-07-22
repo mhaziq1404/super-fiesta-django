5db11defbc04dc0c1fdbcfc67d52342380058230
Muhamad Haziq Muhamad Abdul Hay <mmuhamad@u91z01s02.42kl.edu.my>
2024-07-22 18:46:02 +0800

    Commit again

--- base/models.py
+++ base/models.py
@@ -66,3 +66,15 @@ class Home_Message(models.Model):
 
     def __str__(self):
         return self.body[0:50]
+
+# class Activity(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     body = models.TextField()
+#     updated = models.DateTimeField(auto_now=True)
+#     created = models.DateTimeField(auto_now_add=True)
+
+#     class Meta:
+#         ordering = ['-updated', '-created']
+
+#     def __str__(self):
+#         return self.body[0:50]
